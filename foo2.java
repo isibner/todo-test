d280b15878a4e1d3aac0ae8f18ac6b8d11cf400c
Ian Sibner <sibnerian@gmail.com>
2015-06-21 19:40:05 +0100

    lmao

--- foo2.java
+++ foo2.java
@@ -1,3 +1,9 @@
-// TODO: Things
-// TODO-LABELS: thing1, thing2
-// TODO-BODY: foo bar baz
+// TODO - Stop forgetting the syntax I implement.
+// Labels: stupid, stupid, stupid
+// Honestly, it's pathetic that I can't even remember my own syntax. Like, c'mon, I ought to be better than this.
+
+
+/**
+ * TODO - test out block syntax
+ * This should work in the new world! Exciting!
+ */
